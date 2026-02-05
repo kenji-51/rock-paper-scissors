e9c8b4400e800a957efd3ed3b65d4d3430e225a8
kenji51 <kmori19941103@gmail.com>
2026-02-05 12:09:57 +0900

    じゃんけんプログラムの入力、判定、結果表示を追加

--- rock_paper_scissors_program.rb
+++ rock_paper_scissors_program.rb
@@ -1,25 +1,42 @@
-puts '最初は ぐー、じゃんけん〜〜'
-print '>（ぐー、ちょき、ぱー）のどれかを入力してください：'
+class Janken
+  def initialize
+    @human_hand = gets.chomp
+  end
 
-jyanken_human = gets.chomp
-jyanken_computer = ['ぐー', 'ちょき', 'ぱー'].sample
+  def check_human_hand
+    loop do
+    return @human_hand if ['ぐー', 'ちょき', 'ぱー'].include?(@human_hand)
+    puts '入力が正しくありません。ぐー：ちょき：ぱー のいずれかを入力してください'
+    print '>'
+    @human_hand = gets.chomp
+    end
+  end
 
-puts "コンピューターは、#{jyanken_computer} を出しました。"
+  def rock_paper_scissors
+    @computer_hand = ['ぐー', 'ちょき', 'ぱー'].sample
+    puts "あなたは、#{@human_hand} を出しました"
+    puts "コンピューターは、#{@computer_hand} を出しました"
+  end
 
-if jyanken_human == jyanken_computer
-  puts 'あいこです'
-elsif jyanken_human == 'ぐー' && jyanken_computer == 'ちょき'
-  puts 'やったね、人間の勝ち！！'
-elsif jyanken_human == 'ぐー' && jyanken_computer == 'ぱー'
-  puts '残念、コンピューターの勝ち'
-elsif jyanken_human == 'ちょき' && jyanken_computer == 'ぐー'
-  puts '残念、コンピューターの勝ち'
-elsif jyanken_human == 'ちょき' && jyanken_computer == 'ぱー'
-  puts 'やったね、人間の勝ち！！'
-elsif jyanken_human == 'ぱー' && jyanken_computer == 'ぐー'
-  puts 'やったね、人間の勝ち！！'
-elsif jyanken_human == 'ぱー' && jyanken_computer == 'ちょき'
-  puts '残念、コンピューターの勝ち'
-else
-  puts '入力が正しくありません。ぐー：ちょき：ぱー のいずれかを入力してください'
+  def judge
+    if @human_hand == @computer_hand
+      puts 'あいこです。もう一回'
+    elsif (@human_hand == 'ぐー' && @computer_hand == 'ちょき') ||
+          (@human_hand == 'ちょき' && @computer_hand == 'ぱー') ||
+          (@human_hand == 'ぱー' && @computer_hand == 'ぐー')
+      puts 'やったね人間の勝ち!!'
+    elsif (@human_hand == 'ちょき' && @computer_hand == 'ぐー') ||
+          (@human_hand == 'ぱー' && @computer_hand == 'ちょき') ||
+          (@human_hand == 'ぐー' && @computer_hand == 'ぱー')
+      puts '残念... コンピューターの勝ち'
+    end
+  end
 end
+
+puts '最初は ぐー、じゃんけん〜〜'
+puts '（ぐー、ちょき、ぱー）のどれかを入力してください'
+print '>'
+janken = Janken.new
+janken.check_human_hand
+janken.rock_paper_scissors
+janken.judge
